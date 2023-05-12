713487e801c3d4451a56bba6d515ba5e84e29517
clive-cudi <clivemaina41@gmail.com>
2023-05-12 11:41:30 +0300

    pretty-formatted-records-print

--- client/components/reusable/general/DiagnosisPrint.tsx
+++ client/components/reusable/general/DiagnosisPrint.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styles from "@styles/components/reusable/general/diagnosisPrint.module.scss";
 import { Diagnosis } from "@/types";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
 
 export const DiagnosisPrint = React.forwardRef(({diagnosis}: {diagnosis: Diagnosis[]}, ref: any) => {
+    const session = useSession();
     return (
         <div className={styles.cert_print_wrapper} ref={ref}>
             <style type="text/css" media="print">{"\
@@ -10,12 +13,36 @@ export const DiagnosisPrint = React.forwardRef(({diagnosis}: {diagnosis: Diagnos
             "}</style>
             <div className={styles.cert_content}>
                 {/* logo */}
-                <div></div>
+                <div className={styles.logo}>
+                    <Image src={"/logos/medvista_logo.png"} alt={"@medvista_logo"} fill />
+                </div>
+                <div className={styles.title}>
+                    <h1>Medical records for {session.data?.user.name ?? ""}</h1>
+                </div>
                 <ul>
                     {diagnosis.map((d_) => {
                         return (
                             <li key={d_.diagnosisId}>
-                                {JSON.stringify(d_)}
+                                <div className="diag_info_strip">
+                                    <span>Diagnosis: </span>
+                                    <span>{d_.diagnosis}</span>
+                                </div>
+                                <div className="diag_info_strip">
+                                    <span>Doctor: </span>
+                                    <span>{d_.doctor}</span>
+                                </div>
+                                <div className="diag_info_strip">
+                                    <span>Date: </span>
+                                    <span>{new Date(d_.date).toLocaleDateString()}</span>
+                                </div>
+                                <div className="diag_info_strip">
+                                    <span>Symptoms: </span>
+                                    <span>{d_.symptoms}</span>
+                                </div>
+                                <div className="diag_info_strip">
+                                    <span>Treatment: </span>
+                                    <span>{d_.treatment}</span>
+                                </div>
                             </li>
                         )
                     })}
